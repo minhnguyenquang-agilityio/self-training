9427c95d30786be265352357599e993ab58987f7
Minh Nguyen <minh.nguyen@asnet.com.vn>
2016-10-16 12:46:45 +0700

    add editorconfig gitignore

--- html/trello/gulpfile.js
+++ html/trello/gulpfile.js
@@ -7,39 +7,33 @@ var autoprefixer = require('gulp-autoprefixer');
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'pug'], function() {
 
-    browserSync.init({
-        server: "./dist"
-    });
+  browserSync.init({
+    server: "./dist"
+  });
 
-    gulp.watch("app/sass/*.scss", ['sass']);
+  gulp.watch("app/sass/**/*.scss", ['sass']);
     // gulp.watch("app/template/*.pug").on('change', browserSync.reload);
-    gulp.watch("app/*.pug", ['pug']);
-});
+    gulp.watch("app/**/*.pug", ['pug']);
+  });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    // return gulp.src("app/sass/*.scss")
-    //     .pipe(sass())
-    //     .pipe(gulp.dest("dist/css"))
-    //     .pipe(browserSync.stream());
-
-    return sass("app/sass/*.scss", {sourcemap: true, compass: true})
-        .pipe(autoprefixer({
-          browsers: ['last 2 versions'],
-          cascade: false
-        }))
-        .pipe(gulp.dest("dist/css"))
-        .pipe(browserSync.stream());
-        
+  return sass("app/sass/*.scss", {sourcemap: true, compass: true})
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest("dist/css"))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('pug', function() {
   return gulp.src('app/*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream());
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
