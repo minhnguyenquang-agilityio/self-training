412bc4c8d2c30158e47c6bfbbaea4a47eda15f77
Minh Nguyen <minh.nguyen@asnet.com.vn>
2016-11-17 23:00:46 +0700

    update router

--- angular2/hero/app/app.module.ts
+++ angular2/hero/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports: [
