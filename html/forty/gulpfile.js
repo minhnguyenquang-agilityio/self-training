f0e4f7d4123c6a71665d8cfaad1f78d763c98444
Minh Nguyen <minh.nguyen@asnet.com.vn>
2016-12-27 20:53:54 +0700

    Forty-Init code struct

---
+++ html/forty/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync').create();
+var sass         = require('gulp-ruby-sass');
+var pug          = require('gulp-pug');
+var autoprefixer = require('gulp-autoprefixer');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'pug', 'assets'], function() {
+
+  browserSync.init({
+    server: "./dist"
+  });
+
+  gulp.watch("app/sass/**/*.scss", ['sass']);
+    // gulp.watch("app/template/*.pug").on('change', browserSync.reload);
+    gulp.watch("app/**/*.pug", ['pug']);
+  });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return sass("app/sass/*.scss", {sourcemap: true, compass: true})
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest("dist/css"))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('pug', function() {
+  return gulp.src('app/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('assets', function(){
+  return gulp.src('app/assets/**/*.*')
+    .pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('default', ['serve']);
