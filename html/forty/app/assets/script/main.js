ac0679d2a009038da236a6859d9c2490e426bd6b
Minh Nguyen <minh.nguyen@asnet.com.vn>
2017-01-04 17:43:20 +0700

    Forty-fixed header when scroll

--- html/forty/app/assets/script/main.js
+++ html/forty/app/assets/script/main.js
@@ -1,13 +1,39 @@
 ( function( $ ) {
 
   $(function() {
-    var $body = $('body');
     var $window = $(window);
+    var $body = $('body');
+    var $banner = $('#banner');
+    var $header = $('#header');
 
     $window.on('load pageshow', function() {
       window.setTimeout(function() {
         $body.addClass('loaded');
       }, 100);
+    });
+
+    $window.on('resize', function() {
+      $window.trigger('scroll');
+    });
+
+    $window.on('load', function() {
+      $banner.scrollex({
+        bottom: $header.height() + 10,
+        initialize: function() {
+          $header.addClass('alt');
+        },
+        terminate: function() {
+          $header.removeClass('alt');
+        },
+        enter: function() {
+          $header.addClass('alt');
+          $header.removeClass('reveal');
+        },
+        leave: function() {
+          $header.removeClass('alt');
+          $header.addClass('reveal');
+        }
+      });
     });
 
     if (navigator.appVersion.indexOf("Trident") !== -1) {
