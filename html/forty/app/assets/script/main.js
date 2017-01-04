d1dd154777f11cc54b97a0bfeca73912f8ab6317
Minh Nguyen <minh.nguyen@asnet.com.vn>
2017-01-04 15:39:37 +0700

    Forty-animation for banner and logo when page loaded

--- html/forty/app/assets/script/main.js
+++ html/forty/app/assets/script/main.js
@@ -1,5 +1,18 @@
 ( function( $ ) {
-  if (navigator.appVersion.indexOf("Trident") !== -1) {
-    $('body').addClass('is-ie');
-  }
+
+  $(function() {
+    var $body = $('body');
+    var $window = $(window);
+
+    $window.on('load pageshow', function() {
+      window.setTimeout(function() {
+        $body.addClass('loaded');
+      }, 100);
+    });
+
+    if (navigator.appVersion.indexOf("Trident") !== -1) {
+      $body.addClass('is-ie');
+    }
+
+  });
 } )( jQuery );
