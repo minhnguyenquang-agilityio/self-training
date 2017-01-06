72a6e9051b7ad35a7b69395356b5d06c7acdf6b2
Minh Nguyen <minh.nguyen@asnet.com.vn>
2017-01-06 13:50:53 +0700

    Forty- concat and minify script

--- html/forty/gulpfile.js
+++ html/forty/gulpfile.js
@@ -1,26 +1,28 @@
-// global.isProducton = process.env.ENV === 'prod';
+require('./config');
 
 var gulp         = require('gulp');
 var browserSync  = require('browser-sync').create();
 var sass         = require('gulp-ruby-sass');
 var pug          = require('gulp-pug');
 var autoprefixer = require('gulp-autoprefixer');
-var uglifycss    = require('gulp-uglifycss');
 var cssmin       = require('gulp-cssmin');
 var rename       = require('gulp-rename');
+var replace      = require('gulp-replace');
 var gulpif       = require('gulp-if');
+var concat       = require('gulp-concat');
+var uglify       = require('gulp-uglify');
 
 var condition = process.env.ENV === 'prod';
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'pug', 'assets'], function() {
+gulp.task('serve', ['sass', 'pug', 'assets', 'script'], function() {
 
   browserSync.init({
-    server: "./dist"
+    server: "./dist",
+    port: 3000
   });
 
   gulp.watch("app/sass/**/*.scss", ['sass']);
-  // gulp.watch("app/template/*.pug").on('change', browserSync.reload);
   gulp.watch("app/**/*.pug", ['pug']);
   gulp.watch("app/assets/**/*", ['assets']);
 });
@@ -33,7 +35,8 @@ gulp.task('sass', function() {
     cascade: false
   }))
   .pipe(gulpif(condition, cssmin()))
-  .pipe(gulp.dest("dist/css"))
+  .pipe(gulpif(condition, rename(config.filenames.release.style), rename(config.filenames.build.style)))
+  .pipe(gulp.dest("dist/"))
   .pipe(browserSync.stream());
 });
 
@@ -42,13 +45,28 @@ gulp.task('pug', function() {
   .pipe(pug({
     pretty: true
   }))
+  .pipe(gulpif(condition,
+    replace('<!--styles-->', '<link rel="stylesheet" type="text/css" href="' + config.filenames.release.style + '">'),
+    replace('<!--styles-->', '<link rel="stylesheet" type="text/css" href="' + config.filenames.build.style + '">')))
+  .pipe(gulpif(condition,
+    replace('<!--scripts-->', '<script src="' + config.filenames.release.script + '"></script>'),
+    replace('<!--scripts-->', '<script src="' + config.filenames.build.script + '"></script>')))
   .pipe(gulp.dest('dist'))
   .pipe(browserSync.stream());
 });
 
-gulp.task('assets', function(){
+gulp.task('script', function() {
+  return gulp.src(config.scripts)
+    .pipe(gulpif(condition, concat(config.filenames.release.script), concat(config.filenames.build.script)))
+    .pipe(gulpif(condition, uglify()))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('assets', function() {
   return gulp.src('app/assets/**/*.*')
     .pipe(gulp.dest('dist/assets'));
 });
 
 gulp.task('default', ['serve']);
+
+gulp.task('build', ['sass', 'pug', 'assets', 'script']);
